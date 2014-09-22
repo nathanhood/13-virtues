b38b8e66e7949f777128a7aa2dc094a2abca4b46
Nathan Hood <nathanhood@me.com>
2014-09-22 08:32:40 -0500

    added helper method for daily report button
    * temporarily removed flash message for sign in/sign up
      * was printing sign in message when sign up occurred
    * added edit/delete button to virtues#index - need to configure AJAX

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
     def after_sign_up_path_for(resource)
-      flash.notice = "Welcome to 13 Virtues, #{current_user.email}!"
+      # flash.notice = "Welcome to 13 Virtues, #{current_user.email}!"
       profile_path
     end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def add_daily_report?
+    if current_user.virtues.length > 0
+      if current_user.daily_reports.length > 0
+        if current_user.daily_reports.last.created_at > Date.today
+          return false
+        else
+          return true
+        end
+      else
+        return true
+      end
+    else
+      return false
+    end
+  end
 end
